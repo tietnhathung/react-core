6c5dcefa8f1b1c2294e78339db2b48c1cf430d66
Tiết Nhật Hưng <hungtn@amitech.vn>
2022-05-12 17:15:32 +0700

    clear code

--- api/src/main/java/com/react/api/configuration/AuthEntryPointJwt.java
+++ api/src/main/java/com/react/api/configuration/AuthEntryPointJwt.java
@@ -25,7 +25,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         logger.error("Unauthorized error: {}", authException.getMessage());
         ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED);
         apiError.setMessage(authException.getMessage());
--- api/src/main/java/com/react/api/model/UserRule.java
+++ api/src/main/java/com/react/api/model/UserRule.java
@@ -1,11 +1,16 @@
 package com.react.api.model;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Table(name = "user_rule")
 public class UserRule {
     @Id
@@ -15,9 +20,24 @@ public class UserRule {
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
+    @ToString.Exclude
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "rule_id", nullable = false)
+    @ToString.Exclude
     private Rule rule;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UserRule userRule = (UserRule) o;
+        return id != null && Objects.equals(id, userRule.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
