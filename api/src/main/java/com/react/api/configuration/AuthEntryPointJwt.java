dde85e1cd137016dcff4691a2c484699b98c3750
Tiết Nhật Hưng <hungtn@amitech.vn>
2022-09-12 17:03:04 +0700

    update

--- api/src/main/java/com/react/api/configuration/AuthEntryPointJwt.java
+++ api/src/main/java/com/react/api/configuration/AuthEntryPointJwt.java
@@ -1,40 +1,28 @@
 package com.react.api.configuration;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.react.common.types.ApiResult;
-import com.react.common.types.ApiError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
         logger.error("Unauthorized error: {}", authException.getMessage());
-        ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED);
-        apiError.setMessage(authException.getMessage());
-        ApiResult apiResult = new ApiResult();
-        apiResult.setHttpStatus(HttpStatus.UNAUTHORIZED);
-        apiResult.setErrors(apiError);
-        Gson gson = new GsonBuilder().serializeNulls().create();
-        PrintWriter out = response.getWriter();
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        out.print(gson.toJson(apiResult));
-        out.flush();
+        resolver.resolveException(request, response, null, authException);
     }
 
 }
